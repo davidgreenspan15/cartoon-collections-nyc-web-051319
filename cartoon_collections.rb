59b9f6a587dee5d497abfba720c8a0a2bf9a191d
David Greenspan <ramigreenspan@gmail.com>
2019-04-22 07:22:36 -0400

    cartoon collections

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,35 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves (array)
+counter = 1
+array.each do |name|
+  puts "#{counter}.#{name}"
+  counter +=1
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+  def summon_captain_planet(array)
+    new_array = []
+    i = 0
+    while i < array.length
+      new_array << array[i].capitalize + "!"
+      i += 1
+    end
+    new_array
+  end
+
+def long_planeteer_calls(array)
+  i = 0
+  if  array.any? {|i| i.length > 4}
+    return true
+  else
+    return false
+  i = i + 1
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+def find_the_cheese(array)
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |x|
+    cheese_types.include?(x)
+  end 
 end
